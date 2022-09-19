e66aa66801149dd273bd78ad55ec71aff6fdc91a
Vitor Alves <vitorcarneiro778@gmail.com>
2022-09-19 14:45:28 -0300

    Find Category by id, exception handling

--- src/main/java/com/devsuperior/exercicio/cap1/services/ClientService.java
+++ src/main/java/com/devsuperior/exercicio/cap1/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.exercicio.cap1.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.exercicio.cap1.dto.ClientDTO;
 import com.devsuperior.exercicio.cap1.entities.Client;
 import com.devsuperior.exercicio.cap1.repositories.ClientRepository;
+import com.devsuperior.exercicio.cap1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -23,5 +25,11 @@ public class ClientService {
 		
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 
+	}
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+		return new ClientDTO(entity);
 	}
 }
---
+++ src/main/java/com/devsuperior/exercicio/cap1/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.exercicio.cap1.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
